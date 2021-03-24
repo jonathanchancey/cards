afa9a88a67f25de2fc2b8932299e91f0e0e34986
Jonathan Chancey <jonathan22711@gmail.com>
2021-03-24 15:02:01 -0700

    create new primary directory with fresh node install

---
+++ poker/src/model/table.js
@@ -0,0 +1,30 @@
+
+// pseudo-constants
+var GAME_DELAY = 800; // in ms
+var DEBUG = true;
+
+// gamestate
+var inGame = false;
+var currentTurn = 0;
+var gameOver = false;
+var turnTimer = 15000;
+
+
+
+
+
+// class Game {
+//     constructor(playerNum) {
+//         this.playerNum = playerNum // integer for current player
+//         this.table = this.makeInitialTable()
+//     }
+
+//     makeInitialTable() {
+//         // init the table
+//     }
+
+//     // playCard(){
+
+//     // }
+// }
+
