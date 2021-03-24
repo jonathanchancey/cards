66ed06490183f0c693c9ed532ed5a7533fdb7a36
Jonathan Chancey <jonathan22711@gmail.com>
2021-03-24 12:39:06 -0700

    add basic firebase entry retrieval

--- phase 5/public/app.js
+++ phase 5/public/app.js
@@ -1,14 +1,28 @@
 document.addEventListener("DOMContentLoaded", event => {
     const app = firebase.app();
+
+    const db = firebase.firestore();
+
+    const myPost = db.collection('posts').doc('firepost');
+
+    myPost.get() 
+        .then(doc => { 
+
+            const data = doc.data();
+            document.write( data.title + `<br>`)
+            document.write( data.views )
+            
+        }) // .get is an asynchronous operation that returns a promise
+
 });
 
-function googleLogin() {
-    const provider = new firebase.auth.GoogleAuthProvider();
+// function googleLogin() {
+//     const provider = new firebase.auth.GoogleAuthProvider();
 
-    firebase.auth().signInWithPopup(provider)
-    .then(result => {
-        const user = result.user;
-        document.write(`Hello ${user.displayName}`);
-        console.log(user)
-    }).catch(console.log) // as firebase.auth() is a promise, .then will execute any code on completion
-}
+//     firebase.auth().signInWithPopup(provider)
+//     .then(result => {
+//         const user = result.user;
+//         document.write(`Hello ${user.displayName}`);
+//         console.log(user)
+//     }).catch(console.log) // as firebase.auth() is a promise, .then will execute any code on completion
+// }
