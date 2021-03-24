7ed84d36f7f46ddab00d12d16072962992a4abcb
Jonathan Chancey <jonathan22711@gmail.com>
2021-03-24 11:51:25 -0700

    add assets and firebase chat test

---
+++ phase 2/poker/src/poker/assets/attribution.md
@@ -0,0 +1,9 @@
+Photo by Keenan Constance on Unsplash
+https://unsplash.com/photos/VTLcvV6UVaI
+
+Photo by Amanda Jones on Unsplash
+https://unsplash.com/photos/P787-xixGio
+
+Photo by Amol Tyagi on Unsplash
+https://unsplash.com/photos/7dkLbNE6ycI
+
---
+++ phase 2/poker/src/poker/model/table.js
@@ -0,0 +1,15 @@
+class Game {
+    constructor(playerNum) {
+        this.playerNum = playerNum // integer for current player
+        this.table = this.makeInitialTable()
+    }
+
+    makeInitialTable() {
+        // init the table
+    }
+
+    // playCard(){
+
+    // }
+}
+
