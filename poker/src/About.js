bebc66ba8b675e5eaaa6d5564749f2afaba7b605
Jonathan Chancey <jonathan22711@gmail.com>
2021-03-24 15:55:00 -0700

    create basic website template

--- poker/src/About.js
+++ poker/src/About.js
@@ -3,6 +3,7 @@ import React from 'react'
 export const About = () => (
     <div>
         <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia exercitationem natus expedita quasi impedit ullam cupiditate iure necessitatibus, dolores quam. Reiciendis molestiae inventore sit atque neque consectetur dignissimos modi ducimus?</p>
 
     </div>
 )
--- poker/src/App.js
+++ poker/src/App.js
@@ -2,18 +2,26 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './Home';
 import { About } from './About';
-
+import { NoMatch } from './NoMatch';
+import { Layout } from './components/Layout';
+import {NavBar } from './components/NavBar';
+import { Jumbotron } from './components/Jumbotron';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route path="/about" component={About}/>
-          </Switch>
-        </Router>
+        <NavBar />
+        <Jumbotron />
+          <Layout>
+            <Router>
+              <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route path="/about" component={About}/>
+                <Route component={NoMatch}/>
+              </Switch>
+            </Router>
+          </Layout>
       </React.Fragment>
     );
   }
