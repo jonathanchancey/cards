43ed086671e9a3e7e693908e3c9bf201f5651c7d
Jonathan Chancey <jonathan22711@gmail.com>
2021-03-25 18:24:35 -0700

    add custom table asset

--- poker/src/Table.js
+++ poker/src/Table.js
@@ -1,17 +1,47 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Container } from 'react-bootstrap';
 import { Stage, Layer, Rect, Circle } from 'react-konva';
+import tableSVG from './assets/table.svg';
+import styled from 'styled-components';
+
+
+const Styles = styled.div`
+    .navbar {
+        background-color: #222;
+    }
+
+    .navbar-brand, .navbar-nav .nav-link {
+        color: #bbb;
+
+    }
+
+    &:hover {
+        color: white;
+
+    }
+
+`;
+
 
 export const Table = () => {
   return (
+    <Styles>
+      <img src={tableSVG} alt="My logo" />
+
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Rect width={50} height={50} fill="red" />
+          <Circle x={200} y={200} stroke="black" radius={50} />
+          
+        </Layer>
+      </Stage>
+    </Styles>
     // Stage - is a div wrapper
     // Layer - is an actual 2d canvas element, so you can have several layers inside the stage
     // Rect and Circle are not DOM elements. They are 2d shapes on canvas
-    <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer>
-        <Rect width={50} height={50} fill="red" />
-        <Circle x={200} y={200} stroke="black" radius={50} />
-      </Layer>
-    </Stage>
+
+    
+    
+    
   );
 }
